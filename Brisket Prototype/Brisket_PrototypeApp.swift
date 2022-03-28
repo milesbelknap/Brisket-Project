1efd836363dd38e60b2a402d98fa9c5cbb3b3e07
Miles Belknap <milesbelknap@hotmail.com>
2022-03-28 14:04:49 -0500

    Initial Commit

---
+++ Brisket Prototype/Brisket_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brisket_PrototypeApp.swift
+//  Brisket Prototype
+//
+//  Created by Miles Belknap on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Brisket_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brisket Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brisket Prototype
+//
+//  Created by Miles Belknap on 3/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
